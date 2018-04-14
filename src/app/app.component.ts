af63abf9711d2b4aec6eb4706d58b06279b7392b
jsongo <jsongo@qq.com>
2018-04-14 15:05:19 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 
+interface Img {
+    pic: string,
+    desc: string
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    imgs = [
+    imgs: Img[] = [
         {pic: 'pic1.jpg', desc: '家里随心摆拍'},
         {pic: 'pic2.jpg', desc: '拍摄于深圳南山科技园文化广场'},
         {pic: 'pic3.jpg', desc: '拍摄于上海外滩'},
